f4bf535fd20f2a04d7f3f6ccecb7bd1be2d79b23
unknown <arendt.kamil@wp.pl>
2020-10-28 10:53:19 +0100

    pierwszy commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 12;
+        double f = 4.5;
+        float af = 5.6f;
+        String s = "hello ";
+        boolean b = true;
+        int[] array = {1,2,3,};
+
+        final int c;
+        c = 19;
+
+        // c = 82; nie moze zmienic
+
+        double suma= a+f;
+
+        System.out.println("Dwie zmienne się dodają "+ (suma));
+
+
+
+    }
+}
